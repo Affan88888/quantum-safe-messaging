bd979a9b595b50910d925544332e215729a07b1e
Abdullah Husrep <abdullah.husrep@gmail.com>
2025-04-08 16:26:42 +0200

    homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,27 +1,74 @@
 // src/pages/HomePage.js
-import React from 'react';
-import './HomePage.css'; // Import the CSS file
+import React from "react";
+import Image from "./Unknown.png";
+import Logo from "./logo.png";
+import "./HomePage.css";
 
-const HomePage = () => {
+
+export const WelcomePage = () => {
   return (
-    <div className="container">
-      <h1>Welcome to QuantumSafe Messenger</h1>
-      <p>Your secure, quantum-safe messaging platform.</p>
-      <div className="features">
-        <h2>Key Features:</h2>
-        <ul>
-          <li>End-to-end encryption using quantum-safe algorithms</li>
-          <li>Fast and reliable communication</li>
-          <li>User-friendly interface</li>
-        </ul>
-      </div>
-      <div className="cta">
-        <p>Ready to get started?</p>
-        <a href="/login" className="button">Login</a>
-        <a href="/signup" className="button">Sign Up</a>
+    <div className="welcome-page">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="rectangle" />
+
+          <div className="div" />
+
+          <img
+            className="vecteezy"
+            alt="Vecteezy"
+            src={Image}
+          />
+          
+
+    
+
+          <div className="rectangle-2" />
+          <img className="image" alt="logo" src={Logo} />
+
+          <div className="button">
+            <div className="overlap-group">
+              <div className="text-wrapper">Sign in</div>
+
+            </div>
+          </div>
+
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group">
+              <div className="text-wrapper-2">Log in</div>
+
+            </div>
+          </div>
+
+          <p className="welcome-to-quanttalk">
+            <span className="span">Welcome to Quan</span>
+
+            <span className="text-wrapper-3">tt</span>
+
+            <span className="span">alk Messenger</span>
+          </p>
+
+          <p className="endtoend-encryption">
+            End-to-end encryption using quantum-safe algorithms
+            <br />
+            <br />
+            Fast and reliable communication
+            <br />
+            <br />
+            User-friendly interface
+          </p>
+
+          <p className="your-secure">
+            Your Secure, Quantum-Safe Messaging Platform.
+          </p>
+
+          <div className="get-started">Get Started!</div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default HomePage;
+
+
+export default WelcomePage;
