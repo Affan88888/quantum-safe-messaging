6a3a239c3a12cb8ec666d65965b1edc78f7871d5
ibrahim <ibrahim.djonko@gmail.com>
2025-05-15 21:21:20 +0200

    zavrsio frontend za homepage, login, signup

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,6 +1,9 @@
 // src/pages/HomePage.js
 import React from 'react';
 import './HomePage.css'; // Import the CSS file
+// ovdje importovati jos jednu sliku
+import latticeImage from './background_picture/lattice-based-cryptography-image.png'; // import photos for learn more
+import ckImage from './background_picture/crystals-kyber-image.png';
 
 const HomePage = () => {
   return (
@@ -25,7 +28,7 @@ const HomePage = () => {
             <div className="hero-buttons">
               
               <a href="/signup" className="button get-started">Get Started</a>
-              <a href="#features" className="button learn-more">Learn More</a>
+              <a href="#learn-more" className="button learn-more">Learn More</a>
             </div>
           </div>
           <div className="hero-phone">
@@ -36,8 +39,7 @@ const HomePage = () => {
 
         {/* Features Section */}
         <section className="features">
-          <h2>Key features</h2>
-          {/* napravit cemo kartice da ljepse izgleda */}
+          <h2>Key features</h2> 
           <div className="features-grid">
             <div className="features-card">
               <div className="feature-icon">🔐</div>
@@ -80,6 +82,49 @@ const HomePage = () => {
             <div className="tech-item">
               <div className="tech-logo-oqs"></div>
               <span>OQS Provider</span>
+            </div>
+          </div>
+        </section>
+
+        {/* learn more link button from hero section */}
+        <section id='learn-more' className='learn-more'>
+          {/* Section 1 */}
+          <div className='learn-more-section'>
+            <div className='learn-more-text'>
+              <h2>Open Quantum Safe (OQS) Project</h2>
+              <p>
+                The Open Quantum Safe (OQS) project is an open-source initiative aimed at supporting the development and integration of quantum-resistant cryptographic algorithms. It provides tools like liboqs, a C library for quantum-safe algorithms, and integrations with protocols such as OpenSSL. OQS plays a crucial role in transitioning to cryptographic standards that can withstand quantum computing threats.
+              </p>
+            </div>
+            <div className='learn-more-image'>
+              <img src='path_to_oqs_image' alt='Open Quantum Safe Project' />
+            </div>
+          </div>
+
+          {/* Section 2 */}
+          <div className='learn-more-section reverse'>
+            
+            <div className='learn-more-text'>
+              <h2>Lattice-Based Cryptography</h2>
+              <p>
+                Lattice-based cryptography relies on complex mathematical structures called lattices, which are grids of points in multidimensional space. The security of these cryptographic schemes is based on the difficulty of solving certain problems within these lattices, a challenge even for quantum computers. This makes lattice-based cryptography a strong candidate for post-quantum security.
+              </p>
+            </div>
+            <div className='learn-more-image'>
+              <img src={latticeImage} alt='Lattice-Based Cryptography' />
+            </div>
+          </div>
+
+          {/* Section 3 */}
+          <div className='learn-more-section'>
+            <div className='learn-more-text'>
+              <h2>CRYSTALS-Kyber and CRYSTALS-Dilithium Algorithms</h2>
+              <p>
+                CRYSTALS-Kyber and CRYSTALS-Dilithium are cryptographic algorithms designed to be secure against quantum attacks. Kyber is used for key encapsulation, while Dilithium is used for digital signatures. Both algorithms have been selected by NIST for standardization, highlighting their robustness and efficiency in post-quantum cryptographic applications.
+              </p>
+            </div>
+            <div className='learn-more-image'>
+              <img src={ckImage} alt='CRYSTALS-Kyber and Dilithium' />
             </div>
           </div>
         </section>
